35d5389c11b74d8e2ba9fc0f9963f53ae9221569
Neps <137317675+LegenGiga@users.noreply.github.com>
2025-04-14 15:07:07 +0200

    Testing Workflow v1

--- planet/views.py
+++ planet/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import redirect
 
 from rest_framework.response import Response
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from planet.serializers import PlanetSerializer
 from planet.models import Planet
 
-class PlanetView(ViewSet):
+class PlanetView(ModelViewSet):
     serializer_class = PlanetSerializer
     queryset = Planet.objects.all()
-
-    def list(self, request):
-        planets = Planet.objects.all()
-        serializer = PlanetSerializer(planets, many=True)
-        return Response(serializer.data)
-    
-    def create(self, request):
-        serializer = PlanetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
